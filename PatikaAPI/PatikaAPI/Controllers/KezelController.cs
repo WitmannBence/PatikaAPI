40048b1b0b7b256cb12bd2016a935e15048ae894
WitmannBence <witmannb@kkszki.hu>
2024-12-06 11:07:22 +0100

    gatya

--- PatikaAPI/PatikaAPI/Controllers/KezelController.cs
+++ PatikaAPI/PatikaAPI/Controllers/KezelController.cs
@@ -1,11 +1,27 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PatikaAPI.Models;
 
 namespace PatikaAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class KezelController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            using (var context = new PatikaContext())
+            {
+                try
+                {
+                    List<Kezel> kezelesek = context.Kezels.ToList();
+                    return Ok(kezelesek);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
     }
 }
--- PatikaAPI/PatikaAPI/Models/Kezel.cs
+++ PatikaAPI/PatikaAPI/Models/Kezel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace PatikaAPI.Models;
 
@@ -9,7 +10,9 @@ public partial class Kezel
 
     public int BetegsegId { get; set; }
 
+    [JsonIgnore]
     public virtual Betegseg Betegseg { get; set; } = null!;
 
+    [JsonIgnore]
     public virtual Gyogyszer Gyogyszer { get; set; } = null!;
 }
